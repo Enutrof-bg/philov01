04fcd5ee1354d5d6866357245e35fe6db344c60b
Kevin Wang <kevwang@f6r13s16.clusters.42paris.fr>
2025-07-10 13:27:30 +0200

    up

---
+++ philov09/philo/philo_route.c
@@ -0,0 +1,143 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_route.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevwang <kevwang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 17:37:14 by kevwang           #+#    #+#             */
+/*   Updated: 2025/07/01 17:37:15 by kevwang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	philo_mort(t_philo *philo)
+{
+	long	elapsed;
+	long	time_to_die;
+
+	if (get_int(&philo->je_mange, &philo->full) == 1)
+		return (0);
+	elapsed = get_time_ms() - get_long(&philo->je_mange, &philo->last_meal);
+	time_to_die = philo->table_p->time_to_die / 1000;
+	if (elapsed > time_to_die)
+		return (1);
+	return (0);
+}
+
+void	*dinner_check(void *data)
+{
+	t_table	*table;
+	int		i;
+
+	table = (t_table *)data;
+	while (get_int(&table->table_mutex, &table->nbr_thread) != table->nbr_philo)
+	{
+	}
+	while (get_int(&table->table_mutex, &table->end) == 0)
+	// while (1)
+	{
+		i = 0;
+		while (i < table->nbr_philo
+			&& get_int(&table->table_mutex, &table->end) == 0)
+		{
+			if (get_int(&table->table_mutex, &table->all_full)
+				== table->nbr_philo)
+				set_int(&table->table_mutex, &table->end, 1);
+			if (philo_mort(&table->philo[i]) == 1)
+			{
+				ft_write(&table->philo[i], DEAD);
+				set_int(&table->table_mutex, &table->end, 1);
+			}
+			i++;
+		}
+		// usleep(100);
+	}
+	return (NULL);
+}
+
+void	sim_start_delay(long start_time)
+{
+	while (get_time_ms() < start_time)
+		continue ;
+}
+
+void	*routine(void *data)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)data;
+	while (get_int(&philo->table_p->table_mutex,
+			&philo->table_p->all_ready) == 0)
+	{
+	}
+	// while (philo->table_p->all_ready == 0)
+	// {
+	// }
+	// sim_start_delay(philo->table_p->sync);
+	// while (get_time_ms() < philo->table_p->start)
+	// {
+	// }
+	set_long(&philo->je_mange, &philo->last_meal, get_time_ms());
+	increase_int(&philo->table_p->table_mutex, &philo->table_p->nbr_thread);
+	while (get_int(&philo->table_p->table_mutex,
+			&philo->table_p->end) == 0)
+	{
+		if (philo->full == 1)
+		{
+			increase_int(&philo->table_p->table_mutex,
+				&philo->table_p->all_full);
+			break ;
+		}
+		ft_eat(philo);
+		ft_sleep(philo);
+		ft_think(philo);
+	}
+	return (NULL);
+}
+
+void	*philo_solo(void *data)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)data;
+	set_long(&philo->je_mange, &philo->last_meal, get_time_ms());
+	ft_write(philo, FORK1);
+	while (get_int(&philo->table_p->table_mutex, &philo->table_p->end) == 0)
+	{
+		if (philo_mort(&philo->table_p->philo[0]) == 1)
+		{
+			ft_write(philo, DEAD);
+			set_int(&philo->table_p->table_mutex, &philo->table_p->end, 1);
+		}
+	}
+	return (NULL);
+}
+
+void	start_table(t_table *table)
+{
+	if (table->nbr_philo == 0 || table->nbr_time_to_eat == 0)
+		return ;
+	else if (table->nbr_philo == 1)
+	{
+		if (pthread_create(&table->philo[0].t1, NULL,
+				&philo_solo, (void *)&table->philo[0]) != 0)
+			return ;
+		set_long(&table->table_mutex, &table->start, get_time_ms());
+		if (pthread_join(table->philo[0].t1, NULL) != 0)
+			return ;
+	}
+	else if (table->nbr_philo >= 2)
+	{
+		// set_long(&table->table_mutex, &table->start, get_time_ms());
+		// set_long(&table->table_mutex, &table->sync, get_time_ms());
+		if (ft_create_thread(table) == -1)
+			return ;
+		// set_long(&table->table_mutex, &table->sync, get_time_ms());
+		set_long(&table->table_mutex, &table->start, get_time_ms());
+		set_int(&table->table_mutex, &table->all_ready, 1);
+		if (ft_join_thread(table) == -1)
+			return ;
+	}
+}
---
+++ philov09/philo/philo_time.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_time.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevwang <kevwang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 14:47:19 by kevwang           #+#    #+#             */
+/*   Updated: 2025/06/30 14:47:20 by kevwang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+long	get_time_ms(void)
+{
+	struct timeval	tv;
+
+	if (gettimeofday(&tv, NULL) == -1)
+		return (-1);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+// void	precise_usleep(long usec, t_table *table)
+// {
+// 	long	start;
+// 	long	elapsed;
+// 	long	rem;
+
+// 	start = get_time_ms() * 1000;
+// 	while (get_time_ms() * 1000 - start < usec)
+// 	{
+// 		if (get_int(&table->table_mutex, &table->end) == 1)
+// 			break ;
+// 		elapsed = get_time_ms() * 1000 - start;
+// 		rem = usec - elapsed;
+// 		if (rem > 1000)
+// 		{
+// 			usleep(rem / 2);
+// 		}
+// 		else
+// 		{
+// 			while (get_time_ms() * 1000 - start < usec)
+// 			{
+// 			}
+// 		}
+// 	}
+// }
+void	precise_usleep(long usec, t_table *table)
+{
+	long	start;
+
+	start = get_time_ms() * 1000;
+	while (get_time_ms() * 1000 - start < usec)
+	{
+		if (get_int(&table->table_mutex, &table->end) == 1)
+			break ;
+		usleep(100);
+	}
+}
